ce83f2f4393e83e1310c5b0831356f014814d16b
DK\AMCH <amch@sitecore.net>
2017-01-03 10:35:11 +0200

    commit patch

---
+++ src/Sitecore.Support.98776/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.98776")]
+[assembly: AssemblyProduct("Sitecore.Support.98776")]
+[assembly: ComVisible(false)]
